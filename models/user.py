42b819d91897a8f2aa4bfb4246a57c4c27ce2a47
Ali-TBB <Ali-Tbb@git.com>
2024-03-11 17:56:11 +0100

    airbnb

--- models/user.py
+++ models/user.py
@@ -7,12 +7,11 @@ from models.base_model import BaseModel
 
 class User(BaseModel):
     """User class for representing users."""
+    email = ''
+    password = ''
+    first_name = ''
+    last_name = ''
 
     def __init__(self, *args, **kwargs):
         """Initializes a new instance of the class."""
-
         super().__init__(*args, **kwargs)
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
